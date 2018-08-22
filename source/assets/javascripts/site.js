bdcb0bf4e313271a79e485be8de5efb0464a1293
adrianchinghc <adrian@upstackstudio.com>
2018-08-22 17:38:14 +0800

    Fix 2 navbar item active at the same time

--- source/assets/javascripts/site.js
+++ source/assets/javascripts/site.js
@@ -20,33 +20,33 @@
 		$("#meet-expert i").tooltip();
 
     // custom parallax
-    $(window).scroll(function() {
-      var top = $(window).scrollTop();
-      var width = $(window).width();
-      var bottom = top + $(window).height();
-      if (width > 768) {
-        $("img.background").css({top: -1 * Math.abs(top / 2)});
-      }
-      else {
-        if (top > $(".section.welcome").height()) { $(".section.welcome").css("visibility", "hidden"); }
-        else { $(".section.welcome").css("visibility", "visible"); }
-      }
+    // $(window).scroll(function() {
+    //   var top = $(window).scrollTop();
+    //   var width = $(window).width();
+    //   var bottom = top + $(window).height();
+    //   if (width > 768) {
+    //     $("img.background").css({top: -1 * Math.abs(top / 2)});
+    //   }
+    //   else {
+    //     if (top > $(".section.welcome").height()) { $(".section.welcome").css("visibility", "hidden"); }
+    //     else { $(".section.welcome").css("visibility", "visible"); }
+    //   }
 
-      $(".parallax").each(function() {
-        if ((bottom > $(this).offset().top) && (width > 768)) {
-          if ($(this).offset().top > top ) {
-            var parallax = (Math.abs(top - $(this).offset().top) / 2);
-          }
-          else {
-            var parallax = ($(this).offset().top - top) / 2;
-          }
-          $(this).css("backgroundPosition", ("0px " + (parseInt(parallax)).toString() + "px"));
-        }
-        else {
-          $(this).css("backgroundPosition", "0px 0px");
-        }
-      });
-    });
+    //   $(".parallax").each(function() {
+    //     if ((bottom > $(this).offset().top) && (width > 768)) {
+    //       if ($(this).offset().top > top ) {
+    //         var parallax = (Math.abs(top - $(this).offset().top) / 2);
+    //       }
+    //       else {
+    //         var parallax = ($(this).offset().top - top) / 2;
+    //       }
+    //       $(this).css("backgroundPosition", ("0px " + (parseInt(parallax)).toString() + "px"));
+    //     }
+    //     else {
+    //       $(this).css("backgroundPosition", "0px 0px");
+    //     }
+    //   });
+    // });
 
     // Smooth Scrolling
     $("a.navbar-brand[href^='#'], ul.nav li a[href^='#'], a.scroll-down").click(function(e) {
@@ -60,8 +60,8 @@
     });
 
 		// Add active class to navbar for page specific tab
-		var pathname = window.location.pathname.replace(/\/\s*$/, "");
-		$('.nav > li > a[href="'+pathname+'"]').parent().addClass('active');
+		// var pathname = window.location.pathname.replace(/\/\s*$/, "");
+		// $('.nav > li > a[href="'+pathname+'"]').parent().addClass('active');
 
 	}); // end of document ready
 })(jQuery); // end of jQuery name space
